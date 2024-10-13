ed8b9b025b5dd914c40b5a6809ceceab72d692ef
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-10-13 11:50:40 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { Suspense } from "react";
 import "./App.css";
-
-import Navigation from "./components/Navigation/Navigation";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import CamperPage from "./pages/CampersPage/CampersPage";
-import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
 
 function App() {
   return (
     <div>
-      <Navigation />
-
-      <Suspense fallback={<b>Loading page...</b>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CamperPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="*" element={<HomePage />} />
-        </Routes>
-      </Suspense>
+      <p>HOME</p>
     </div>
   );
 }
--- src/utils/formatPrice.js
+++
@@ -1,4 +0,0 @@
-export const formatPrice = (price) => {
-  const priceInDecimal = price.toFixed(2);
-  return priceInDecimal.replace(".", ",");
-};
